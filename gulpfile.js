d81aee871cb55d1aca01106ade105a44e41eb6a2
ievavold <ievavold@wisc.edu>
2017-09-15 12:33:01 -0500

    add build process

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp')
+const rollup = require('rollup')
+const typescript = require('rollup-plugin-typescript')
+
+gulp.task('build', async () => {
+  const bundle = await rollup.rollup({
+    input: './minesweeper.tsx',
+    external: [
+      'react',
+      'react-dom',
+    ],
+    plugins: [
+      typescript(),
+    ],
+  })
+
+  await bundle.write({
+    file: './minesweeper.js',
+    format: 'iife',
+    globals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+    },
+    name: 'minesweeper',
+    sourcemap: true,
+  })
+})
+
+gulp.task('watch', ['build'], async () => {
+  gulp.watch('./minesweeper.tsx', ['build'])
+})
